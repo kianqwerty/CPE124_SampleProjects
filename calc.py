6535b0d18361378ea27548f9aedd5beb017c8a22
kianqwerty <88371486+kianqwerty@users.noreply.github.com>
2022-12-23 21:03:40 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -92,7 +92,16 @@ class Ui_Dialog(object):
         self.zeroButton.setText(_translate("Dialog", "0"))
         self.label.setText(_translate("Dialog", "0"))
         self.equalsButton.setText(_translate("Dialog", "="))
-
+    
+    def press_it(self, pressed):
+	    if pressed == "C":
+		    self.outputLabel.setText("0")
+	    else:
+		    #Check to see if it starts with 0 and delete 0
+		    if self.outputLabel.text() == "0":
+			    self.outputLabel.setText("")
+		    #Concatenate the pressed button with what was there already
+		    self.outputLabel.setText(f'{self.outputLabel.text()}{pressed}')
 
 if __name__ == "__main__":
     import sys
